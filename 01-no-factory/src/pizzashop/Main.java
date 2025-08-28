98967490e7094f812c0e288a28fa98afe5ebd37c
Hadi <hadi.rezaie2015@gmail.com>
2025-08-28 22:32:01 +0430

    implimentations used in main method.

---
+++ 01-no-factory/src/pizzashop/Main.java
@@ -0,0 +1,16 @@
+package pizzashop;
+
+public class Main {
+    public static void main(String[] args) {
+        PizzaStore store = new PizzaStore();
+
+        System.out.println("=== Ordering Cheese Pizza ===");
+        Pizza pizza1 = store.orderPizza("cheese");
+
+        System.out.println("\n=== Ordering Pepperoni Pizza ===");
+        Pizza pizza2 = store.orderPizza("pepperoni");
+
+        System.out.println("\n=== Ordering Veggie Pizza ===");
+        Pizza pizza3 = store.orderPizza("veggie");
+    }
+}
