5ea096eb4a7d8d9d48f0fe9037e803a820a65fc4
Hadi <hadi.rezaie2015@gmail.com>
2025-08-29 10:06:32 +0430

    all classes used in the main method in order to prepare different types of pizza.

--- 02-simple-factory/src/pizzashop/Main.java
+++ 02-simple-factory/src/pizzashop/Main.java
@@ -4,5 +4,26 @@ package pizzashop;
 public class Main {
     public static void main(String[] args) {
 
+        SimplePizzaFactory factory = new SimplePizzaFactory();
+        PizzaStore store = new PizzaStore(factory);
+
+        System.out.println("=== Ordering Clam Pizza ===");
+        Pizza pizza = store.orderPizza("clam");
+        System.out.println("\n\n END \n\n");
+
+        System.out.println("=== Ordering Pepperoni Pizza ===");
+        Pizza pizza2 = store.orderPizza("pepperoni");
+        System.out.println("\n\n END \n\n");
+
+        System.out.println("=== Ordering Cheese Pizza ===");
+        Pizza pizza3 = store.orderPizza("cheese");
+        System.out.println("\n\n END \n\n");
+
+        System.out.println("=== Ordering Veggie Pizza ===");
+        Pizza pizza4 = store.orderPizza("veggie");
+        System.out.println("\n\n END \n\n");
+
+        System.out.println("\n=== Ordering Unknown Pizza ===");
+        Pizza pizzaN = store.orderPizza("unknown");
     }
 }
