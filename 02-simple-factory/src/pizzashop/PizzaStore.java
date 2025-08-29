a700b842a03760b6d7a02078475530e185d99ff9
Hadi <hadi.rezaie2015@gmail.com>
2025-08-29 10:00:53 +0430

    PizzaStore class created.

---
+++ 02-simple-factory/src/pizzashop/PizzaStore.java
@@ -0,0 +1,24 @@
+package pizzashop;
+
+public class PizzaStore {
+    private SimplePizzaFactory factory;
+
+    public PizzaStore(SimplePizzaFactory factory) {
+        this.factory = factory;
+    }
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza = factory.createPizza(type);
+
+        if (pizza != null) {
+            pizza.prepare();
+            pizza.bake();
+            pizza.cut();
+            pizza.box();
+        } else {
+            System.out.println("Sorry, we don't have this type of pizza!");
+        }
+
+        return pizza;
+    }
+}
