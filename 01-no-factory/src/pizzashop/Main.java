f751f808fd735b5a75ea6656b0d326eff872669e
Hadi <hadi.rezaie2015@gmail.com>
2025-08-29 09:32:58 +0430

    updated.

--- 01-no-factory/src/pizzashop/Main.java
+++ 01-no-factory/src/pizzashop/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         PizzaStore store = new PizzaStore();
 
-        System.out.println("=== Ordering Cheese Pizza ===");
+        System.out.println("\n\n=== Ordering Cheese Pizza ===");
         Pizza pizza1 = store.orderPizza("cheese");
 
         System.out.println("\n=== Ordering Pepperoni Pizza ===");
